f1f5cf42169bfe7d231f7c84e43c4e29a45a0b14
AnSannikova <asyadol15@gmail.com>
2024-04-07 18:45:25 +0300

    refactor(index.html, index.tsx, modal.tsx): добавить счетчик яндекс.метрики

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from 'react';
 import styles from './modal.module.scss';
+import useMetrika from '../../hooks/useMetrika';
 export default function Modal() {
 	const [isOpen, setIsOpen] = useState<boolean>(false);
+	const { ym } = useMetrika();
 
 	useEffect(() => {
 		const handleEsc = (e: KeyboardEvent) => {
@@ -14,9 +16,14 @@ export default function Modal() {
 		document.addEventListener('keydown', handleEsc);
 	}, []);
 
+	const handleButton = () => {
+		setIsOpen(true);
+		ym('reachGoal', 'click');
+	};
+
 	return (
 		<div className={styles.wrapper}>
-			<button onClick={() => setIsOpen(true)}>Открыть модальное окно</button>
+			<button onClick={handleButton}>Открыть модальное окно</button>
 			<div className={styles.modal}>
 				{isOpen && (
 					<div className={styles.container}>
